dcc632d95d34d9e3366d6dad5e1cdc05d2c0a3ec
shwethashankar <shwethagsa@gmail.com>
2019-02-06 11:12:05 +0530

    Removed config file and added .env support

--- config/index.js
+++
@@ -1,8 +0,0 @@
-/**
-This file exports a merged JSON object of all the required configuration files.
-If the NODE_ENV is undefined development is assumed as default environment.
-There is no need to actually load the configuration files directly.
-*/
-
-var config = require("./config")(process.env.NODE_ENV || "development");
-module.exports = config;
